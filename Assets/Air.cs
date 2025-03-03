4e48a3727ef4886b90a636046155913b938db18f
taezaza-chai <taezazachai@gmail.com>
2025-03-03 11:49:37 +0700

    add script

---
+++ Assets/Air.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Air : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float enginePower = 20f;
+    [SerializeField] private float liftBooster = 0.5f;
+    [SerializeField] float darg = 0.001f;
+    [SerializeField] float angularDrag = 0.001f;
+    [SerializeField]  float yawPower = 50f;
+    [SerializeField] float pitchPower = 50f;
+
+    [SerializeField] private float rollPower = 50f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * enginePower);
+            Vector3 lift = Vector3.Project(rb.linearVelocity, transform.forward);
+            rb.AddForce(transform.up * lift.magnitude * liftBooster);
+            rb.linearDamping = rb.linearVelocity.magnitude * darg;
+            rb.angularDamping = rb.linearVelocity.magnitude * angularDrag;
+
+            float yaw = Input.GetAxis("Horizontal") * yawPower;
+            float pitch = Input.GetAxis("Vertical") * pitchPower;
+            float roll = Input.GetAxis("roll") * rollPower;
+            
+            rb.AddForce(transform.up * yaw);
+            rb.AddForce(transform.right * pitch);
+            rb.AddForce(transform.forward * roll);
+        }
+    }
+}
